cae2062fca634a9e05f0bba19b8e1052b9eb275b
Jim Garlick <garlick.jim@gmail.com>
2016-12-21 04:39:48 +0000

    cancon: bidirectional I/O support with libev

--- examples/cancon.c
+++ examples/cancon.c
@@ -5,15 +5,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <termios.h>
+#include <ev.h>
 #include "canmgr_proto.h"
 #include "canmgr_dump.h"
 #include "lxcan.h"
 
 static const uint8_t mynode = 0; // lie
 static const uint8_t mymod = 0; // lie
-static int object = CANOBJ_TARGET_CONSOLESEND;
+static int myobject = CANOBJ_TARGET_CONSOLESEND;
 static int s;
 static int m, n;
+
+static int expecting_ack = 0;
+
+static struct termios saved_tio;
+static struct termios new_tio;
+
+static ev_io can_watcher;
+static ev_io stdin_watcher;
+static struct ev_loop *loop;
 
 void canmgr_ack (struct canmgr_frame *fr, int type)
 {
@@ -45,16 +56,81 @@ void console_request (int obj)
     in.object = obj;
     in.data[0] = mymod;
     in.data[1] = mynode;
-    in.data[2] = object;
+    in.data[2] = myobject;
     in.dlen = 3;
     if (lxcan_send (s, &in) < 0) {
         fprintf (stderr, "lxcan_send: %m\n");
         exit (1);
     }
-    /* FIXME collect ack */
 }
 
-void canobj_console_recv (struct canmgr_frame *fr)
+void canobj_consoleconn (struct canmgr_frame *fr)
+{
+    switch (fr->type) {
+        case CANMGR_TYPE_WO:
+        case CANMGR_TYPE_RO:
+        case CANMGR_TYPE_DAT:
+            goto nak;
+        case CANMGR_TYPE_NAK:
+            fprintf (stderr, "Got NAK to CONSOLECONN\n");
+        case CANMGR_TYPE_ACK:
+            expecting_ack = 0;
+            ev_io_start (loop, &stdin_watcher);
+            break;
+        default:
+            break;
+    }
+    return;
+nak:
+    canmgr_ack (fr, CANMGR_TYPE_NAK);
+}
+
+void canobj_consoledisc (struct canmgr_frame *fr)
+{
+    switch (fr->type) {
+        case CANMGR_TYPE_WO:
+        case CANMGR_TYPE_RO:
+        case CANMGR_TYPE_DAT:
+            goto nak;
+        case CANMGR_TYPE_NAK:
+            fprintf (stderr, "Got NAK to CONSOLEDISC\n");
+        case CANMGR_TYPE_ACK:
+            if (tcsetattr (0, TCSAFLUSH, &saved_tio) < 0) {
+                fprintf (stderr, "tcsetattr: %m\n");
+                exit (1);
+            }
+            exit (0);
+            break;
+        default:
+            break;
+    }
+    return;
+nak:
+    canmgr_ack (fr, CANMGR_TYPE_NAK);
+}
+
+void canobj_consolering (struct canmgr_frame *fr)
+{
+    switch (fr->type) {
+        case CANMGR_TYPE_WO:
+        case CANMGR_TYPE_RO:
+        case CANMGR_TYPE_DAT:
+            goto nak;
+        case CANMGR_TYPE_NAK:
+            fprintf (stderr, "Got NAK to CONSOLERING\n");
+        case CANMGR_TYPE_ACK:
+            expecting_ack = 0;
+            ev_io_start (loop, &stdin_watcher);
+            break;
+        default:
+            break;
+    }
+    return;
+nak:
+    canmgr_ack (fr, CANMGR_TYPE_NAK);
+}
+
+void canobj_consolesend (struct canmgr_frame *fr)
 {
     switch (fr->type) {
         case CANMGR_TYPE_WO:
@@ -73,10 +149,92 @@ nak:
     canmgr_ack (fr, CANMGR_TYPE_NAK);
 }
 
+void canobj_consolerecv (struct canmgr_frame *fr)
+{
+    switch (fr->type) {
+        case CANMGR_TYPE_WO:
+        case CANMGR_TYPE_RO:
+        case CANMGR_TYPE_DAT:
+            goto nak;
+        case CANMGR_TYPE_NAK:
+            fprintf (stderr, "Got NAK to CONSOLERECV\n");
+        case CANMGR_TYPE_ACK:
+            expecting_ack = 0;
+            ev_io_start (loop, &stdin_watcher);
+            break;
+        default:
+            break;
+    }
+    return;
+nak:
+    canmgr_ack (fr, CANMGR_TYPE_NAK);
+}
+
+
+static void stdin_cb (EV_P_ ev_io *w, int revents)
+{
+    struct canmgr_frame in;
+    int len, max = canmgr_maxdata (CANOBJ_TARGET_CONSOLERECV);
+
+    in.pri = 1;
+    in.dst = n | 0x10;
+    in.src = mynode;
+
+    in.xpri = 1;
+    in.type = CANMGR_TYPE_DAT;
+    in.node = in.dst;
+    in.module = m;
+    in.object = CANOBJ_TARGET_CONSOLERECV;
+
+    if ((len = read (0, in.data, max)) <= 0)
+        return; // FIXME handle EOF/error
+    in.dlen = len;
+
+    if (lxcan_send (s, &in) < 0)  {
+        fprintf (stderr, "lxcan_send: %m\n");
+        exit (1);
+    }
+    expecting_ack = 1; 
+    ev_io_stop (loop, &stdin_watcher);
+}
+
+static void can_cb (EV_P_ ev_io *w, int revents)
+{
+    struct canmgr_frame fr;
+
+    if (lxcan_recv (s, &fr) < 0) {
+        fprintf (stderr, "lxcan_recv: %m\n");
+        return;
+    }
+    if (fr.dst != mynode)
+        return; // not addressed to me
+    switch (fr.object) {
+        case CANOBJ_TARGET_CONSOLECONN:
+            canobj_consoleconn (&fr);
+            break;
+        case CANOBJ_TARGET_CONSOLEDISC:
+            canobj_consoledisc (&fr);
+            break;
+        case CANOBJ_TARGET_CONSOLERING:
+            canobj_consolering (&fr);
+            break;
+        case CANOBJ_TARGET_CONSOLERECV:
+            canobj_consolerecv (&fr);
+            break;
+        default:
+            if (fr.object == myobject) {
+                canobj_consolesend (&fr);
+                break;
+            }
+            break;
+    }
+
+}
+
 void sigint_handler (int arg)
 {
     console_request (CANOBJ_TARGET_CONSOLEDISC);
-    exit (0);
+    ev_io_stop (loop, &stdin_watcher);
 }
 
 int main (int argc, char *argv[])
@@ -92,8 +250,6 @@ int main (int argc, char *argv[])
         fprintf (stderr, "improperly specified target\n");
         exit (1);
     }
-    /* construct console object (payload)
-     */
 
     // FIXME: object should be unique
     // Use CANOBJ_TARGET_CONSOLESEND for the common case,
@@ -103,27 +259,30 @@ int main (int argc, char *argv[])
         fprintf (stderr, "lxcan_open: %m\n");
         exit (1);
     }
+    if (tcgetattr (0, &saved_tio) < 0) {
+        fprintf (stderr, "tcgetattr: %m\n");
+        exit (1);
+    }
+    new_tio = saved_tio;
+    cfmakeraw(&new_tio);
+    new_tio.c_cc[VMIN] = 1;
+    new_tio.c_cc[VTIME] = 0;
+    if (tcsetattr (0, TCSAFLUSH, &new_tio) < 0) {
+        fprintf (stderr, "tcsetattr: %m\n");
+        exit (1);
+    }
+
+    loop = EV_DEFAULT;
+    ev_io_init (&can_watcher, can_cb, s, EV_READ);
+    ev_io_init (&stdin_watcher, stdin_cb, 0, EV_READ);
+    ev_io_start (loop, &can_watcher);
+
     console_request (CANOBJ_TARGET_CONSOLECONN);
+    expecting_ack = 1;
 
     signal (SIGINT, sigint_handler);
 
-    for (;;) {
-        if (lxcan_recv (s, &out) < 0) {
-            fprintf (stderr, "lxcan_recv: %m\n");
-            exit (1);
-        }
-        if (out.dst != mynode)
-            continue;
-        switch (out.object) {
-            default:
-                if (out.object == object) {
-                    canobj_console_recv (&out);
-                    break;
-                }
-                break;
-        }
-    }
-    console_request (CANOBJ_TARGET_CONSOLEDISC);
+    ev_run (loop, 0);
     lxcan_close (s);
 
     exit (0);
