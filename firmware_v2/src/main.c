a22bd4c820b9f66d5bfdb8358980c3ea0ee8e37f
Jim Garlick <garlick.jim@gmail.com>
2017-01-05 04:08:02 +0000

    firmware_v2: blink the LD2 LED on Nucleo F091RC board

--- firmware_v2/src/main.c
+++ firmware_v2/src/main.c
@@ -1,12 +1,38 @@
-/*
-  Main file template from stm32tool
-*/
-
 #include <stm32f0xx.h>
+
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_rcc.h>
+#include <stm32f0xx_hal_gpio.h>
 
 int main(void)
 {
-  /* Your code here */
+    GPIO_InitTypeDef GPIO_Init;
 
-  return 0;
+    HAL_Init();
+
+    __GPIOA_CLK_ENABLE();
+
+    GPIO_Init.Pin = GPIO_PIN_5;
+    GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Init.Pull = GPIO_PULLUP;
+    GPIO_Init.Speed = GPIO_SPEED_HIGH;
+
+    HAL_GPIO_Init (GPIOA, &GPIO_Init);
+
+    while (1) {
+        //HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
+        HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+        HAL_Delay (500);
+        HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        HAL_Delay (500);
+    }
 }
+
+void SysTick_Handler (void)
+{
+    HAL_IncTick ();
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
