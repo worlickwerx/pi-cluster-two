42ac57a50bd5c8e1ac1fe06d91103bad68763cd8
Jim Garlick <garlick.jim@gmail.com>
2017-02-10 00:37:26 +0000

    firmware_v2: configure LEDs

--- firmware_v2/src/main.c
+++ firmware_v2/src/main.c
@@ -11,19 +11,43 @@ int main(void)
     HAL_Init();
 
     __GPIOA_CLK_ENABLE();
+    __GPIOC_CLK_ENABLE();
 
+    // PA5 = nucleo LD2
     GPIO_Init.Pin = GPIO_PIN_5;
     GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_Init.Pull = GPIO_PULLUP;
     GPIO_Init.Speed = GPIO_SPEED_HIGH;
-
     HAL_GPIO_Init (GPIOA, &GPIO_Init);
 
+    // PC5 = PWR-ON
+    GPIO_Init.Pin = GPIO_PIN_5;
+    GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Init.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init (GPIOC, &GPIO_Init);
+
+    // PC6 = CAN-ACT
+    GPIO_Init.Pin = GPIO_PIN_6;
+    GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Init.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init (GPIOC, &GPIO_Init);
+
+    // PC7 = IDENTIFY
+    GPIO_Init.Pin = GPIO_PIN_7;
+    GPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Init.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init (GPIOC, &GPIO_Init);
+
+    // set initial PWR-ON state
+    HAL_GPIO_WritePin (GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);
+
     while (1) {
-        //HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
         HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
         HAL_Delay (500);
         HAL_GPIO_WritePin (GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin (GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
         HAL_Delay (500);
     }
 }
