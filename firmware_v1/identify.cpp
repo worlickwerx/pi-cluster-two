3bf62c7f0bd4db902c32700fd745203344d6638a
Jim Garlick <garlick.jim@gmail.com>
2017-02-10 00:36:37 +0000

    firmware_v1: identify 0=off, 1=on, 2=blink

--- firmware_v1/identify.cpp
+++ firmware_v1/identify.cpp
@@ -6,7 +6,7 @@ const uint8_t identify_pin = 14;
 const uint16_t identify_period = 1000; // millisec
 elapsedMillis identify_since;
 
-uint8_t identify_enable = 0; // disabled
+uint8_t identify_mode = 0; // 0=off, 1=on, 2=blink
 uint8_t identify_state = LOW; // LED off
 
 void identify_setup (void)
@@ -21,21 +21,21 @@ void identify_finalize (void)
 
 void identify_set (uint8_t val)
 {
-    identify_enable = val;
-    identify_state = identify_enable ? HIGH : LOW;
+    identify_mode = val;
+    identify_state = identify_mode ? HIGH : LOW;
     digitalWriteFast (identify_pin, identify_state);
     identify_since -= identify_since;
 }
 
 void identify_get (uint8_t *val)
 {
-    *val = identify_enable;
+    *val = identify_mode;
 }
 
 void identify_update (void)
 {
     if (identify_since >= identify_period) {
-        if (identify_enable) {
+        if (identify_mode == 2) {
             identify_state = (identify_state == LOW) ? HIGH : LOW;
             digitalWriteFast (identify_pin, identify_state);
         }
