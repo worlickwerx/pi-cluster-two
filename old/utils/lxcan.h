3b9a81350b5e4d9a9daf28b6a32a1227e640cd6d
Jim Garlick <garlick.jim@gmail.com>
2020-07-11 11:16:22 -0700

    old: removed
    Drop code now split off to pi-cluster-one project.

--- old/utils/lxcan.h
+++
@@ -1,9 +0,0 @@
-int lxcan_open (const char *name);
-void lxcan_close (int s);
-
-int lxcan_send (int s, struct canmgr_frame *fr);
-int lxcan_recv (int s, struct canmgr_frame *fr);
-
-/*
- * vi:tabstop=4 shiftwidth=4 expandtab
- */
--- old/utils/monotime.h
+++
@@ -1,6 +0,0 @@
-double monotime (void);
-double monotime_since (double t);
-
-/*
- * vi:tabstop=4 shiftwidth=4 expandtab
- */
