8ddcf02aa1a526e0b60b1231be0bf53499bce085
Jim Garlick <garlick.jim@gmail.com>
2021-01-17 08:20:03 -0800

    bramble: add skeleton sub-project
    Add an automake project that will contain user space tools
    and libraries for accessing pi hardware such as CAN, I2C,
    and barrier network.
    
    Enable libtool, and go with single Makefile.am at the top rather
    than one per directory.

---
+++ bramble/src/cmd/bramble.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+#include "src/libbramble/bramble.h"
+
+int main (int argc, char *argva[])
+{
+    return 0;
+}
+
+/*
+ * vi:ts=4 sw=4 expandtab
+ */
+
---
+++ bramble/src/libbramble/bramble.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef _BRAMBLE_H
+#define _BRAMBLE_H
+
+#endif /* _BRAMBLE_H */
+
+/*
+ * vi:ts=4 sw=4 expandtab
+ */
