87ad6a80c05fff63703799620a829e2c91980d81
Jim Garlick <garlick.jim@gmail.com>
2021-01-22 13:15:59 -0800

    bramble/cansnoop: use libbramble utils functions

--- bramble/src/cmd/cansnoop.c
+++ bramble/src/cmd/cansnoop.c
@@ -9,26 +9,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
-#include <time.h>
 #include <ctype.h>
 
 #include "src/libbramble/bramble.h"
 
-static double monotime (void)
-{
-    struct timespec ts;
-
-    clock_gettime (CLOCK_MONOTONIC, &ts);
-    return (double)ts.tv_sec + (double)ts.tv_nsec * 1E-9;
-}
-
-static int usage (void)
-{
-    fprintf (stderr, "Usage: bramble cansnoop\n");
-    return 1;
-}
-
-int cansnoop_main (int argc, char *argva[])
+int cansnoop_main (int argc, char *argv[])
 {
     int fd;
     struct canmsg_raw raw;
@@ -39,12 +24,10 @@ int cansnoop_main (int argc, char *argva[])
     int i;
 
     if (argc != 1)
-        return usage ();
-    fd = can_open (BRAMBLE_CAN_INTERFACE);
-    if (fd < 0) {
-        fprintf (stderr, "%s: %s", BRAMBLE_CAN_INTERFACE, strerror (errno));
-        return 1;
-    }
+        die ("Usage: bramble cansnoop\n");
+    if ((fd = can_open (BRAMBLE_CAN_INTERFACE)) < 0)
+        die ("%s: %s\n", BRAMBLE_CAN_INTERFACE, strerror (errno));
+
     while (can_recv (fd, &raw) == 0) {
         if (canmsg_v1_decode (&raw, &msg) < 0) {
             fprintf (stderr, "could not decode canmsg_v1 frame\n");
@@ -61,7 +44,7 @@ int cansnoop_main (int argc, char *argva[])
         }
 
         printf ("%07.3f %.3x->%.3x  %-4s %-12s %-23s %s%.*s%s\n",
-                monotime () - t_start,
+                monotime_since (t_start),
                 msg.src,
                 msg.dst,
                 canmsg_v1_typestr (&msg),
