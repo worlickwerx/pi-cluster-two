726d1194a8690b3801be2cd8bb61f2e53a8290fc
Jim Garlick <garlick.jim@gmail.com>
2021-01-23 08:30:44 -0800

    bramble/firmware-version: use libbramble utils functions

--- bramble/src/cmd/firmware-version.c
+++ bramble/src/cmd/firmware-version.c
@@ -11,32 +11,22 @@
 
 #include "src/libbramble/bramble.h"
 
-static int usage (void)
-{
-    fprintf (stderr, "Usage: bramble firmware-version\n");
-    return 1;
-}
-
-int firmware_version_main (int argc, char *argva[])
+int firmware_version_main (int argc, char *argv[])
 {
     int fd;
     uint8_t version;
 
     if (argc != 1)
-        return usage ();
-    fd = i2c_open (BRAMBLE_I2C_DEVICE, I2C_ADDRESS);
-    if (fd < 0) {
-        fprintf (stderr, "%s:0x%x: %s",
-                 BRAMBLE_I2C_DEVICE, I2C_ADDRESS, strerror (errno));
-        return 1;
-    }
-    if (i2c_read (fd, I2C_REG_VERSION, &version, 1) < 0) {
-        fprintf (stderr, "%s:0x%x: could not read register %d: %s",
-                 BRAMBLE_I2C_DEVICE, I2C_ADDRESS, I2C_REG_VERSION,
-                 strerror (errno));
-        return 1;
-    }
+        die ("Usage: bramble firmware-version\n");
+
+    if ((fd = i2c_open (BRAMBLE_I2C_DEVICE, I2C_ADDRESS)) < 0)
+        die ("%s: %s\n", BRAMBLE_I2C_DEVICE, strerror (errno));
+
+    if (i2c_read (fd, I2C_REG_VERSION, &version, 1) < 0)
+        die ("i2c read: %s\n", strerror (errno));
+
     printf ("%d\n", version);
+
     close (fd);
     return 0;
 }
