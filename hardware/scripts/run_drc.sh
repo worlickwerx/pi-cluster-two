858fd856de706b4337c7ee1955fae7580c972a19
Jim Garlick <garlick.jim@gmail.com>
2024-09-21 10:21:44 -0700

    testsuite: ignore warnings in CI DRC
    Problem: CI starts failing out of the blue with lib_footprint_mismatch
    warnings even though design files have not been updated.
    
    This is probably because kicad is updated in CI and newer versions
    of footprints are available.
    
    Limit the CI check to errors only.

--- hardware/scripts/run_drc.sh
+++ hardware/scripts/run_drc.sh
@@ -3,7 +3,7 @@
 kicad-cli pcb drc \
     -o pcb_drc.rpt \
     --exit-code-violations \
-    --severity-all \
+    --severity-error \
     $DRC_INPUT
 result=$?
 test $result -eq 0 || cat pcb_drc.rpt >&2
